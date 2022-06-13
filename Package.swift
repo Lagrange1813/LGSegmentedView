f2a3aacfec54ca7d29206070f4e79ab1dd443ff4
Lagrange1813 <Lagrange1813@163.com>
2022-06-13 15:51:36 +0800

    Without SnapKit

--- Package.swift
+++ Package.swift
@@ -17,14 +17,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-	.package(url: "https://github.com/SnapKit/SnapKit", from: "5.6.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LGSegmentedView",
-            dependencies: ["SnapKit"]),
+            dependencies: []),
         .testTarget(
             name: "LGSegmentedViewTests",
             dependencies: ["LGSegmentedView"]),
